62ed0f70a94e88a1552985361e01db59530c74bf
Seba <seb.santis@duocuc.cl>
2026-06-19 00:27:19 -0400

    Arreglo de errores

--- src/main/java/com/Usuario/usuario/config/WebClientConfiguration.java
+++ src/main/java/com/Usuario/usuario/config/WebClientConfiguration.java
@@ -1,15 +1,20 @@
 package com.Usuario.usuario.config;
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.http.HttpHeaders;
+import java.util.Base64;
 
 @Configuration
 public class WebClientConfiguration {
-    @LoadBalanced
+
     @Bean
     public WebClient.Builder webClientBuilder() {
-        return WebClient.builder();
-}
+        String credentials = Base64.getEncoder()
+                .encodeToString("admin:admin123".getBytes());
+
+        return WebClient.builder()
+                .defaultHeader(HttpHeaders.AUTHORIZATION, "Basic " + credentials);
     }
+}
